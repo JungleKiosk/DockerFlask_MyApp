3936669c979a71050a28256811533f72789cc632
Fra <135766845+JungleKiosk@users.noreply.github.com>
2024-04-14 00:29:41 +0200

    run Docker Compose

--- back_end/main.py
+++ back_end/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from os import environ
 
 app = Flask(__name__)
 
@@ -7,4 +8,4 @@ def home():
     return 'Hello Flak'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+        app.run(host='0.0.0.0',port=environ.get('PORT'),debug=environ.get('DEBUG'))
